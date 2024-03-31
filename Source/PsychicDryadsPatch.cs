2affb9a8395ec86c7208370548a7879e98f19503
Gareth <blackfireweb@gmail.com>
2024-03-31 17:29:49 +0100

    Update to add the fixtures

--- Source/PsychicDryadsPatch.cs
+++ Source/PsychicDryadsPatch.cs
@@ -7,9 +7,6 @@ namespace PsychicDryadsPatch
     {
         public Mod(ModContentPack content) : base(content)
         {
-#if DEBUG
-            Harmony.DEBUG = true;
-#endif
             new Harmony("Garethp.rimworld.PsychicDryadsPatch.main").PatchAll();
         }
     }
